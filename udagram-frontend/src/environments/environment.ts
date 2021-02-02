7edc39f783c023a3a30addaa679f510a184c6f73
ianguest <iag.1.apps@gmail.com>
2021-02-02 15:49:52 +0000

    revert changes to environment files

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,8 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: process.env.API_HOST
-  //apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
